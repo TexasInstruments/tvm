e8fd57bd2d9d47ff9df3dc8bd7b610dd6231e585
Ajay Jayaraj <ajayj@ti.com>
2022-05-04 09:48:47 -0500

    TVM+TIDL: Update build script to create TVM python package

--- ti_build.sh
+++ ti_build.sh
@@ -22,8 +22,13 @@ CLANG_VERSION=clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04
 
 BUILD_DIR=${WORKSPACE}/build
 
+# Build TVM
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 cd $BUILD_DIR
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DUSE_MICRO=ON -DUSE_SORT=ON -DUSE_TIDL=ON -DUSE_LLVM=${TVM_DEPS_PATH}/$CLANG_VERSION/bin/llvm-config -DUSE_TIDL_RT_PATH=$(ls -d ${PSDKR_PATH}/tidl_j7*/ti_dl/rt) -DUSE_TIDL_PSDKR_PATH=${PSDKR_PATH} ..
 make -j$(nproc)
+
+# Create the python package
+cd ../python
+python3 ./setup.py bdist_wheel
