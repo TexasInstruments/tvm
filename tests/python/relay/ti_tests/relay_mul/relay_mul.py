7bcf55c350c90c3db0a4d5ade263aed9c451ee76
Ajay Jayaraj <ajayj@ti.com>
2022-06-22 14:27:01 -0500

    TVM+TIDL: Refactor relay_mul test case
    Move relay_mul into its own folder and use compile_relay from
    compile_model.py.
    
    CODEGEN-10114

--- tests/python/relay/ti_tests/relay_mul/relay_mul.py
+++ tests/python/relay/ti_tests/relay_mul/relay_mul.py
@@ -7,133 +7,16 @@ import logging
 from typing import List
 import numpy as np
 
-
-def relay_compile(model_name, mod_orig, params, model_input_list, args, max_num_subgraphs=16):
-    """ Compile a model in Relay IR graph
-
-    Parameters
-    ----------
-    model_name : string
-        Name of the model
-    mod_orig : tvm.relay.Module
-        Original Relay IR graph
-    params : dict of str to tvm.NDArray
-        The parameter dict to be used by relay
-    model_input_list : list of dictionary for multiple calibration data
-        A dictionary where the key in input name and the value is input tensor
-    max_num_subgraphs : int
-        Max number of subgraphs to offload to TIDL
-    Returns
-    -------
-    status: int
-        Status of compilation:
-            1  - compilation for TIDL offload succeeded
-            -1 - compilation for TIDL offload failed - failure for CI testing
-            0  - no compilation due to missing TIDL tools or ARM64 GCC tools
-    """
-
-    from tvm.relay.backend.contrib import tidl
-    from tvm import relay
-
-    def get_tidl_tools_path():
-        tidl_tools_path = os.getenv("TIDL_TOOLS_PATH")
-        if tidl_tools_path is None:
-            raise Exception("Environment variable TIDL_TOOLS_PATH is not set!")
-        relay_import_lib = os.path.join(tidl_tools_path, "tidl_model_import_relay.so")
-        if not os.path.exists(relay_import_lib):
-            raise Exception("${TIDL_TOOLS_PATH}/tidl_model_import_relay.so does not exist!")
-        return tidl_tools_path
-
-    def get_arm_compiler():
-        arm_gcc_path = os.getenv("ARM64_GCC_PATH")
-        if arm_gcc_path is None:
-            raise Exception("Environment variable ARM64_GCC_PATH is not set!")
-        arm_gcc = os.path.join(arm_gcc_path, "bin", "aarch64-none-linux-gnu-g++")
-        if not os.path.exists(arm_gcc):
-            raise Exception("${ARM64_GCC_PATH}/aarch64-none-linux-gnu-g++ does not exist!")
-        return arm_gcc
-
-    def get_c7x_compiler_path():
-        cgt7x_root = os.getenv("CGT7X_ROOT")
-        if cgt7x_root is None:
-            raise Exception("Environment variable CGT7X_ROOT is not set!")
-        cl7x_bin = os.path.join(cgt7x_root, "bin", "cl7x")
-        if not os.path.exists(cl7x_bin):
-            raise Exception("${CGT7X_ROOT}/cl7x does not exist!")
-        return cgt7x_root
-
-
-
-    c7x_codegen = 1 if model_name.endswith('_c7x') else 0
-
-    try:
-        tidl_tools_path = get_tidl_tools_path()
-        if args.target:
-            arm_gcc = get_arm_compiler()
-        if c7x_codegen == 1:
-            cgt7x_root = get_c7x_compiler_path()
-            #c7x_codegen = 9
-    except Exception as ex:
-        print(f"{__file__}: Skip compilation because: {ex}")
-        return 0
-
-    tidl_artifacts_folder = "./artifacts_" + model_name +  ("_target" if args.target else "_host")
-    os.makedirs(tidl_artifacts_folder, exist_ok = True)
-    for root, dirs, files in os.walk(tidl_artifacts_folder, topdown=False):
-        for f in files:
-            os.remove(os.path.join(root, f))
-        for d in dirs:
-            os.rmdir(os.path.join(root, d))
-    tidl_compiler = tidl.TIDLCompiler(platform="J7", version="8.2",
-                                      tidl_tools_path=tidl_tools_path,
-                                      artifacts_folder=tidl_artifacts_folder,
-                                      tensor_bits=16,
-                                      max_num_subgraphs=max_num_subgraphs,
-                                      deny_list=args.denylist,
-                                      c7x_codegen=c7x_codegen,
-                                      accuracy_level=0,
-                                      advanced_options={'calibration_iterations': 10}
-                                     )
-
-    if args.nooffload:
-        mod, status = mod_orig, 0
-    else:
-        mod, status = tidl_compiler.enable(mod_orig, params, model_input_list)
-
-    if status == 1: # TIDL compilation succeeded
-        print("Graph execution with TIDL")
-    else: # TIDL compilation failed or no TIDL compilation due to missing tools
-        print("Graph execution without TIDL")
-
-    if args.target:
-        target = "llvm -device=arm_cpu -mtriple=aarch64-linux-gnu"
-    else:
-        target = "llvm"
-
-    with tidl.build_config(tidl_compiler=tidl_compiler):
-        graph, lib, params = relay.build_module.build(mod, target=target, params=params)
-    tidl.remove_tidl_params(params)
-
-    path_lib = os.path.join(tidl_artifacts_folder, "deploy_lib.so")
-    path_graph = os.path.join(tidl_artifacts_folder, "deploy_graph.json")
-    path_params = os.path.join(tidl_artifacts_folder, "deploy_param.params")
-    if args.target:
-        lib.export_library(path_lib, cc=arm_gcc)
-    else:
-        lib.export_library(path_lib)
-    with open(path_graph, "w") as fo:
-        fo.write(graph)
-    with open(path_params, "wb") as fo:
-        fo.write(relay.save_param_dict(params))
-
-    print("Artifacts can be found at " + tidl_artifacts_folder)
-    return status
-
-
 def compile_model(model_name, args) -> List:
     """Create a relay model, compile it and save input/reference output to file"""
+    # Add '..' to Python modules path to import compile_relay
+    import sys
+    sys.path.append("..")
+
     import tvm
     from tvm import relay
+
+    from compile_model import compile_relay
 
     # define graph in relay
     a_shape = (672, 14, 14)
@@ -157,7 +40,11 @@ def compile_model(model_name, args) -> List:
     input_variables = ["a", "b"]
     input_values = [a_data, b_data]
     input_dict_list = dict(zip(input_variables, input_values))
-    status = relay_compile(model_name, mod, params, input_dict_list, args, max_num_subgraphs=0)
+
+    artifacts_folder = "./artifacts_" + model_name +  ("_target" if args.target else "_host")
+    status = compile_relay(mod, params, input_dict_list, "J7",
+                          is_target=True, w_tidl=False, w_c7x=True, artifacts_folder=artifacts_folder, tidl_bits=8)
+
     if status != 1:
         print("TIDL compilation failed")
         sys.exit(1)
@@ -171,8 +58,7 @@ def compile_model(model_name, args) -> List:
     save_to_file("b_data", b_data)
     save_to_file("reference_output", reference_output.asnumpy())
 
-    artifacts_dir = "artifacts_" + model_name + ("_target" if args.target else "_host")
-    return ["a_data", "b_data", "reference_output", artifacts_dir]
+    return ["a_data", "b_data", "reference_output", artifacts_folder]
 
 def copy_to_evm(model:str, ip: str, artifacts: List) -> bool:
     import subprocess
--- ti_build.sh
+++ ti_build.sh
@@ -31,7 +31,7 @@ BUILD_DIR=${WORKSPACE}/build
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 cd $BUILD_DIR
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DUSE_MICRO=ON -DUSE_SORT=ON -DUSE_TIDL=ON -DUSE_LLVM=${TVM_DEPS_PATH}/$CLANG_VERSION/bin/llvm-config -DUSE_TIDL_RT_PATH=$(ls -d ${PSDKR_PATH}/tidl_j7*/ti_dl/rt) -DUSE_TIDL_PSDKR_PATH=${PSDKR_PATH} ..
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DUSE_MICRO=ON -DUSE_SORT=ON -DUSE_TIDL=ON -DUSE_LLVM="${TVM_DEPS_PATH}/$CLANG_VERSION/bin/llvm-config --link-static" -DHIDE_PRIVATE_SYMBOLS=ON -DUSE_TIDL_RT_PATH=$(ls -d ${PSDKR_PATH}/tidl_j7*/ti_dl/rt) -DUSE_TIDL_PSDKR_PATH=${PSDKR_PATH} ..
 make -j$(nproc)
 
 # Create the python package
@@ -39,20 +39,26 @@ cd ../python
 python3 ./setup.py bdist_wheel
 cd -
 
-# Use TVM to compile unit tests
-UNIT_TEST_DIR=${WORKSPACE}/tests/python/relay/ti_tests/unit_tests
-cd $UNIT_TEST_DIR
+# Use TVM to compile a unit test
+TEST_DIR=${WORKSPACE}/tests/python/relay/ti_tests/relay_mul
+cd $TEST_DIR
 
 # Create a Python virtual environment to install the TVM wheel file and build a test case
 unset PYTHONPATH
 python3 -m venv build_env && source ./build_env/bin/activate
 export https_proxy=http://wwwgate.ti.com:80
 export http_proxy=http://wwwgate.ti.com:80
-pip3 install ${WORKSPACE}/python/dist/tvm-*-cp36-cp36m-linux_x86_64.whl
-pip3 install graphviz
 
-# Use TVM python artifacts from the whl package
-echo $PYTHONPATH
+# Update pip3 first
+python3 -m pip install -U pip
+
+# Install TVM in the venv
+pip3 install ${WORKSPACE}/python/dist/tvm-*-cp36-cp36m-linux_x86_64.whl
+
+# Install packages required to compile models with TVM
+pip3 install graphviz
+pip3 install tflite==2.4.0 onnx==1.9.0 mxnet==1.7.0.post2 gluoncv==0.8.0 torch==1.10.2 tensorflow==1.14.0 timm==0.5.4
+pip3 install --no-deps torchvision==0.11.2
 
 # Compile model on host, generate artifacts directory and copy to EVM
 ./relay_mul.py --compile --copy_to_evm ${EVM_IP}
